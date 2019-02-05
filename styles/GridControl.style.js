26503b788ba4fa6ff7cabcd8194cf1f31821a628
Edoardo Sabadelli <edoardo@dhis2.org>
2019-02-05 09:45:35 +0000

    ab687d8 Fix arrow handling in user list for mentions

---
+++ styles/GridControl.style.js
@@ -0,0 +1,15 @@
+export default {
+    gridContainer: {
+        marginTop: 6
+    },
+    gridItem: {
+        marginRight: 15
+    },
+    label: {
+        marginBottom: 5,
+        fontSize: 12
+    },
+    placeholder: {
+        color: 'rgba(0, 0, 0, 0.38)'
+    }
+};
