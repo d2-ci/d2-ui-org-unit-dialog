e0f55bc77b14042a70c6a616ba1d498b2c9aa68c
GitHub <noreply@github.com>
2020-01-29 09:07:58 +0000

    9e38ae4 fix: fix interpretation comment delete (#543)

---
+++ styles/UserOrgUnitsPanel.js
@@ -0,0 +1,51 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = {
+    container: {
+        paddingLeft: 18,
+        background: '#F4F5F8',
+        margin: '0 0px 10px',
+        WebkitUserSelect: 'none',
+        KhtmlUserSelect: 'none',
+        MozUserSelect: 'none',
+        MsUserSelect: 'none',
+        UserSelect: 'none'
+    },
+    gridItem: {
+        marginRight: 40
+    },
+    checkbox: {
+        fontSize: 10,
+        position: 'relative',
+        top: 2,
+        color: 'transparent',
+        background: 'white',
+        border: '1px solid #dedede',
+        borderRadius: 3
+    },
+    checkboxChecked: {
+        fontSize: 14,
+        marginBottom: -2,
+        marginRight: -2,
+        position: 'relative',
+        top: 1,
+        right: 1
+    },
+    stopIcon: {
+        position: 'relative',
+        top: 5,
+        color: '#a6a7a6',
+        fontSize: '14px',
+        margin: '0px 3px 0px -9px'
+    },
+    text: {
+        fontSize: '90%',
+        cursor: 'pointer',
+        color: '#000',
+        position: 'relative',
+        top: '3px'
+    }
+};
