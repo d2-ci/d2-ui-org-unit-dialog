161def5592093d558edc8d5b22aef7dad60bbfb7
GitHub <noreply@github.com>
2020-05-14 08:49:29 +0000

    1470d3f fix: translations for pagination labels in FavoritesDialog (#613)

---
+++ styles/GridControl.style.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = {
+    gridContainer: {
+        marginTop: 6
+    },
+    gridItem: {
+        marginRight: 15
+    },
+    label: {
+        marginBottom: 5,
+        fontSize: 12
+    },
+    placeholder: {
+        color: 'rgba(0, 0, 0, 0.38)'
+    }
+};
