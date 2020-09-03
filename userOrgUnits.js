b5544b5473fc7fc63aa72d3a6de3ecc75258b5d9
Edoardo Sabadelli <edoardo@dhis2.org>
2020-09-03 13:24:35 +0000

    e9bf9e1 v7.0.6

---
+++ userOrgUnits.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _d2I18n = require('@dhis2/d2-i18n');
+
+var _d2I18n2 = _interopRequireDefault(_d2I18n);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = [{ displayName: _d2I18n2.default.t('User organisation unit'), id: 'USER_ORGUNIT' }, { displayName: _d2I18n2.default.t('User sub-units'), id: 'USER_ORGUNIT_CHILDREN' }, { displayName: _d2I18n2.default.t('User sub-x2-units'), id: 'USER_ORGUNIT_GRANDCHILDREN' }];
